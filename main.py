4114c8507442b6d787fcb6693a0687d0a1001bcd
Łukasz Pietrzak <lukasz30105@gmail.com>
2024-01-04 17:24:25 +0100

    dodano poruszajacego sie gracza

--- main.py
+++ main.py
@@ -15,11 +15,43 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((25, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+    def update(self, pressed_keys):
+        # move player
+        if pressed_keys[K_w]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_s]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_a]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_d]:
+            self.rect.move_ip(5, 0)
+
+        # keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 class Game():
     def __init__(self):
         # create sprite lists
         self.all_sprites_list = pygame.sprite.Group()
         self.enemies_sprites_list = pygame.sprite.Group()
+
+        self.player = Player()
+        self.all_sprites_list.add(self.player)
+
         self.game_over = False
 
     def process_events(self):
@@ -37,14 +69,21 @@ class Game():
 
     def run_logic(self):
         """method to update positions and check for collisions"""
-        pass
+
+        # Get the set of keys pressed and check for user input
+        pressed_keys = pygame.key.get_pressed()
+        self.player.update(pressed_keys)
 
     def display_frame(self, screen):
-
+        """Display everything on the screen"""
         screen.fill((0, 0, 0))
         if self.game_over:
             font = pygame.font.SysFont("Serif", 25)
             text = font.render("Game over", True, (0, 0, 0))
+        else:
+            # draw all sprites
+            for entity in self.all_sprites_list:
+                screen.blit(entity.surf, entity.rect)
 
         pygame.display.flip()
 
@@ -62,9 +101,9 @@ def main():
 
     while running:
         running = game.process_events()
+        game.run_logic()
         game.display_frame(screen)
         clock.tick(60)
-        print(running)
 
     pygame.quit()
 
