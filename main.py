78ce13ce8580fb90e0afd4d6629b4f91ea89c191
Łukasz Pietrzak <lukasz30105@gmail.com>
2024-01-05 21:14:21 +0100

    move math functions to different file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from math_functions import count_angle
 
 from pygame.locals import (
     K_ESCAPE,
@@ -66,47 +67,12 @@ class Enemy(pygame.sprite.Sprite):
 
         self.speed = 5
 
-    def distance_points(self, point1, point2):
-        """measure distance between two points"""
-        x1, y1 = point1
-        x2, y2 = point2
-        d = math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2)
-        distance = math.sqrt(d)
-
-        return distance
-
-    def count_angle(self, point1, point2):
-        """returns angle of triangle, in which point1 and point2 make hypotenuse"""
-
-        x1, y1 = point1
-        x2, y2 = point2
-
-        # measure triangle sides
-        adjacent = x2 - x1
-        # avoid dividing by zero
-        if adjacent == 0:
-            return math.radians(90)
-
-        opposite = y2 - y1
-
-        # count tangent
-        tan = opposite / adjacent
-        # find angle using arctangent
-        angle = math.atan(tan)
-        angle = math.degrees(angle)
-
-        # prevent enemy wobbling when angle is value close to 0
-        if -2 < angle < 2:
-            angle = 0
-
-        return angle
-
     def update(self, player_position):
         # get coordinates of player and enemy
         point1 = player_position[:2]
         point2 = self.rect[:2]
 
-        angle = self.count_angle(point1, point2)
+        angle = count_angle(point1, point2)
         cosinus = math.cos(angle)
         sinus = math.sin(angle)
 
